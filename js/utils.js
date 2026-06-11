ddd682011570794ddef3831d2cd9f8111b5237a1
thiogi <thyogenesf@gmail.com>
2026-06-11 19:53:46 -0300

    Delete js/utils.js

--- js/utils.js
+++
@@ -1,9 +0,0 @@
-function getSalonIdFromURL() {
-  const path = window.location.pathname;
-  const parts = path.split("/s/");
-  return parts[1] || null;
-}
-
-function todayTimestamp() {
-  return new Date().getTime();
-}
